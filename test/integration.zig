47dd9853260826b9a30ee925a4c2790999c16d3e
Chris Birster <christopher.birster@gmail.com>
2026-08-18 19:00:30 -0400

    BONGO-0024 add advanced operation options

--- test/integration.zig
+++ test/integration.zig
@@ -24,4 +24,5 @@ test {
     _ = @import("integration/23_write_concern.zig");
     _ = @import("integration/24_read_concern.zig");
     _ = @import("integration/25_find_options.zig");
+    _ = @import("integration/26_operation_options.zig");
 }
